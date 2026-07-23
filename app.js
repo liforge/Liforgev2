4bd453afcd31d2eaba7ae8ced9d35f936466f503
liforge <liforge.app@protonmail.com>
2026-07-23 17:35:15 +0200

    Remove alert for building values
    Remove alert for building values in app.js

--- app.js
+++ app.js
@@ -982,7 +982,6 @@ function renderBuilding(){
     const chartWidth = chart.clientWidth;
 
   const values = getBuildingValues();
-  alert(JSON.stringify(values));
 
   const layout = createBuildingLayout(chartWidth);
 
